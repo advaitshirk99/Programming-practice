b153b09e27421f04347378b9d7579a29ce627422
advaitshirk99 <shirvaikar.msismpl2022@learner.manipal.edu>
2022-09-22 14:35:40 +0530

    program 4

--- Day 4/program 4/header.h
+++ Day 4/program 4/header.h
@@ -1,4 +1,6 @@
 int *get_array(int size);
 void display_array(int *arr, int size);
 
+int search(int *arr, int element, int size);
+int insert(int *arr, int element, int position); //return the current position of i in the pointer array
 int *set_union(int *arr1, int size1, int *arr2, int size2);
--- Day 4/program 4/operation.c
+++ Day 4/program 4/operation.c
@@ -23,4 +23,27 @@ void display_array(int *arr, int size){
 	printf("\n");
 }
 
-int *set_union(int *arr1, int size1, int *arr2, int size2);
+int search(int *arr, int element, int size){
+
+	int i=0, found = 0;
+	
+	while (found != 1){
+		
+		if(*(arr+i) == element){
+
+			return 1;
+		}
+
+		else if (size ==i) return 0;
+		
+		else i++;
+	}
+}
+
+int insert(int *arr, int element, int position);
+int *set_union(int *arr1, int size1, int *arr2, int size2){
+
+	
+	int i, j, *union_array;
+
+}
