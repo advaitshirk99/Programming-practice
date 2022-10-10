12199318191d6016ba7caccfc37a2208c865a456
advaitshirk99 <shirvaikar.msismpl2022@learner.manipal.edu>
2022-10-10 11:42:23 +0530

    program 11 fixed

--- Day 4/program 11/main.c
+++ Day 4/program 11/main.c
@@ -5,7 +5,7 @@ int main(){
 
 	char string[1000];
 	printf("Enter the string:\n");
-	scanf("%s", string);
+	fgets(string, 1000, stdin);
 
 	printf("The converted string with all uppercase characters is:\n");
 	lower_to_upper(string);
