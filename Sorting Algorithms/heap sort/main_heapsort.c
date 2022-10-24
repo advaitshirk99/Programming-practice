af3f15eff63fccf200afd90167cd14dbc991583f
advshirk99 <adshirvaikar@gmail.com>
2022-10-24 20:47:56 +0530

    Added extract_max to heapsort.c

--- Sorting Algorithms/heap sort/main_heapsort.c
+++ Sorting Algorithms/heap sort/main_heapsort.c
@@ -4,9 +4,11 @@
 int main(){
 
     int arr[] = {12, 31, 53, 429, 1, 218, 85, 13, 68, 192};
-    int i, n = 10;
-
+    int max_element, i, n = 10;
+    
     heapSort(arr, n);
-    printf("The sorted array is:\n");
+    printf("\nThe sorted array is:\n");
     for(i = 0; i<n; i++) printf("%d ", arr[i]);
+
+    printf("\nMax element is: %d\n", extract_max(arr, n));
 }
